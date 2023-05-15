d0622b27a17e7009ee45f7eced1265d9c7a41fa3
Abdu Zoghbi <a.zoghbi@nasa.gov>
2023-05-15 11:54:22 -0400

    rename back utils to misc

--- tests/aztools/test_misc.py
+++ tests/aztools/test_misc.py
@@ -2,42 +2,42 @@ import unittest
 
 import numpy as np
 
-from aztools import utils
+from aztools import misc
 
 
-class UtilsTest(unittest.TestCase):
-    """testing aztools utils"""
+class MiscTest(unittest.TestCase):
+    """testing aztools misc"""
 
     def test_split_array_1(self):
         """simple splitting
         t = [0,1,2,3,4,5]
         """
         tarr = np.arange(6, dtype=np.double)
-        sarr,_ = utils.split_array(tarr, 2)
+        sarr,_ = misc.split_array(tarr, 2)
         barr = [[0,1], [2,3], [4,5]]
         for idx,_sarr in enumerate(sarr):
             np.testing.assert_array_almost_equal(_sarr, barr[idx])
 
         # strict=False #
-        sarr,_ = utils.split_array(tarr, 4, strict=False)
+        sarr,_ = misc.split_array(tarr, 4, strict=False)
         barr = [[0,1,2,3], [4,5]]
         for idx,_sarr in enumerate(sarr):
             np.testing.assert_array_almost_equal(_sarr, barr[idx])
 
         # strict = True #
-        sarr,_ = utils.split_array(tarr, 4, strict=True)
+        sarr,_ = misc.split_array(tarr, 4, strict=True)
         barr = [[0,1,2,3]]
         for idx,_sarr in enumerate(sarr):
             np.testing.assert_array_almost_equal(_sarr, barr[idx])
 
         # approx = True #
-        sarr,_ = utils.split_array(tarr, 4, approx=True)
+        sarr,_ = misc.split_array(tarr, 4, approx=True)
         barr = [[0,1,2], [3,4,5]]
         for idx,_sarr in enumerate(sarr):
             np.testing.assert_array_almost_equal(_sarr, barr[idx])
 
         # approx = True #
-        sarr,_ = utils.split_array(tarr, 5, approx=True)
+        sarr,_ = misc.split_array(tarr, 5, approx=True)
         barr = [[0,1,2,3,4,5]]
         for idx,_sarr in enumerate(sarr):
             np.testing.assert_array_almost_equal(_sarr, barr[idx])
@@ -49,32 +49,32 @@ class UtilsTest(unittest.TestCase):
         """
         tarr = np.arange(6, dtype=np.double)
         tarr[2] = np.nan
-        sarr,_ = utils.split_array(tarr, 2)
+        sarr,_ = misc.split_array(tarr, 2)
         barr = [[0,1], [3,4], [5]]
         for idx,_sarr in enumerate(sarr):
             np.testing.assert_array_almost_equal(_sarr, barr[idx])
 
         # approx=true #
-        sarr,_ = utils.split_array(tarr, 2, approx=True)
+        sarr,_ = misc.split_array(tarr, 2, approx=True)
         barr = [[0,1], [3,4], [5]]
         for idx,_sarr in enumerate(sarr):
             np.testing.assert_array_almost_equal(_sarr, barr[idx])
 
         # strict=true #
-        sarr,_ = utils.split_array(tarr, 2, strict=1)
+        sarr,_ = misc.split_array(tarr, 2, strict=1)
         barr = [[0,1], [3,4]]
         for idx,_sarr in enumerate(sarr):
             np.testing.assert_array_almost_equal(_sarr, barr[idx])
 
         # another nan at index=0 #
         tarr[0] = np.nan
-        sarr,_ = utils.split_array(tarr, 2)
+        sarr,_ = misc.split_array(tarr, 2)
         barr = [[1], [3,4], [5]]
         for idx,_sarr in enumerate(sarr):
             np.testing.assert_array_almost_equal(_sarr, barr[idx])
 
         # split_at_gaps = False #
-        sarr,_ = utils.split_array(tarr, 2, split_at_gaps=0)
+        sarr,_ = misc.split_array(tarr, 2, split_at_gaps=0)
         barr = [[np.nan, 1], [np.nan,3], [4,5]]
         for idx,_sarr in enumerate(sarr):
             np.testing.assert_array_almost_equal(_sarr, barr[idx])
@@ -86,13 +86,13 @@ class UtilsTest(unittest.TestCase):
         """
         tarr = np.arange(6, dtype=np.double)
         tarr[2] = np.nan
-        sarr,_ = utils.split_array(tarr, 2, overlap=1)
+        sarr,_ = misc.split_array(tarr, 2, overlap=1)
         barr = [[0,1], [1], [3,4], [4,5], [5]]
         for idx,_sarr in enumerate(sarr):
             np.testing.assert_array_almost_equal(_sarr, barr[idx])
 
         # strict = 1 #
-        sarr,_ = utils.split_array(tarr, 2, overlap=1, strict=1)
+        sarr,_ = misc.split_array(tarr, 2, overlap=1, strict=1)
         barr = [[0,1], [3,4], [4,5]]
         for idx,_sarr in enumerate(sarr):
             np.testing.assert_array_almost_equal(_sarr, barr[idx])
@@ -102,17 +102,17 @@ class UtilsTest(unittest.TestCase):
         # [0,1,2,3,4,5,6,7,8]
 
         xarr = np.arange(9)
-        igrp = utils.group_array(xarr, by_n=[1, 1])
+        igrp = misc.group_array(xarr, by_n=[1, 1])
         grp = [[idx] for idx in xarr]
         for idx,_igrp in enumerate(igrp):
             np.testing.assert_array_almost_equal(_igrp, grp[idx])
 
-        igrp = utils.group_array(xarr, by_n=[4, 1])
+        igrp = misc.group_array(xarr, by_n=[4, 1])
         grp = [[0,1,2,3], [4,5,6,7], [8]]
         for idx,_igrp in enumerate(igrp):
             np.testing.assert_array_almost_equal(_igrp, grp[idx])
 
-        igrp = utils.group_array(xarr, by_n=[3, 2])
+        igrp = misc.group_array(xarr, by_n=[3, 2])
         grp = [[0,1,2], [3,4,5,6,7,8]]
         for idx,_igrp in enumerate(igrp):
             np.testing.assert_array_almost_equal(_igrp, grp[idx])
@@ -125,12 +125,12 @@ class UtilsTest(unittest.TestCase):
         xarr = np.arange(9)
         xarr[1] = 0
         xarr[5] = 4
-        igrp = utils.group_array(xarr, by_n=[1, 1])
+        igrp = misc.group_array(xarr, by_n=[1, 1])
         grp = [[0,1],[2],[3],[4,5],[6],[7],[8]]
         for idx,_igrp in enumerate(igrp):
             np.testing.assert_array_almost_equal(_igrp, grp[idx])
 
-        igrp = utils.group_array(xarr, by_n=[4, 1])
+        igrp = misc.group_array(xarr, by_n=[4, 1])
         grp = [[0,1,2,3], [4,5,6,7], [8]]
         for idx,_igrp in enumerate(igrp):
             np.testing.assert_array_almost_equal(_igrp, grp[idx])
@@ -138,7 +138,7 @@ class UtilsTest(unittest.TestCase):
         # [0,0,0,0,4,4,6,7,8]
         xarr[2] = 0
         xarr[3] = 0
-        igrp = utils.group_array(xarr, by_n=[2, 1.5])
+        igrp = misc.group_array(xarr, by_n=[2, 1.5])
         grp = [[0,1,2,3],[4,5,6], [7,8]]
         for idx,_igrp in enumerate(igrp):
             np.testing.assert_array_almost_equal(_igrp, grp[idx])
@@ -150,7 +150,7 @@ class UtilsTest(unittest.TestCase):
         xarr = np.arange(9)
         xarr[1] = 0
         xarr[5] = 4
-        igrp = utils.group_array(xarr, bins=[0, 4, 8])
+        igrp = misc.group_array(xarr, bins=[0, 4, 8])
         grp = [range(4), [4,5,6,7]]
         for idx,_igrp in enumerate(igrp):
             np.testing.assert_array_almost_equal(_igrp, grp[idx])
